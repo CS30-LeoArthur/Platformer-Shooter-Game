2a2febc486ab1e84538b80b81156c93d0f731fc2
Leology0 <leology42@gmail.com>
2023-12-13 13:30:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,32 @@ BLUE = (0, 0, 255)
 SCREENWIDTH = 800
 SCREENHEIGHT = 800
 
+class Player():
+    def __init__(self, x, y, width, height, xChange, yChange, health):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.xChange = xChange
+        self.yChange = yChange
+        self.health = health
+    
+    def drawPlayer(self, screen):
+        pygame.draw.rect(screen, RED, [self.x, self.y, self.width, self.height])
+
+class Platform():
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    
+    def drawPlatform(self, screen):
+        pygame.draw.rect(screen, GREY, [self.x, self.y, self.width, self.height])
+
+
+
+
 def main():
     pygame.init()
     
@@ -15,16 +41,21 @@ def main():
     screen = pygame.display.set_mode(size)
 
     clock = pygame.time.Clock()
-
+    
+    # Loop
     done = False
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
 
+        # Logic
+        
+        # Drawing
         screen.fill(WHITE)
 
         pygame.display.flip()
+        clock.tick(60)
 
     pygame.quit()
 
